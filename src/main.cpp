34bdc4a7f719a5d23c69021b0843c1a19fe5354a
Maquinadelmal23110112 <159966461+Maquinadelmal23110112@users.noreply.github.com>
2024-04-23 00:22:25 +0000

    Version5

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,9 @@
 #include <string.h>
 #include <thread>
 #include <ftxui/dom/elements.hpp>
-#include <ftxiu/screen/screen.hpp>
-#include <ftxiu/screen/string.hpp>
-#include <ftxiu/screen/terminal.hpp>
+#include <ftxui/screen/screen.hpp>
+#include <ftxui/screen/string.hpp>
+#include <ftxui/screen/terminal.hpp>
 #include <fstream>
 
 using namespace std;
@@ -12,33 +12,29 @@ using namespace ftxui;
 
 int main(int argc, char const *argv[])
 {
-    fstream archivo;
-    archivo.open("./assets/images/canon.txt");
-    string canon;
-    archivo>>canon;
-    archivo.close();
+    Dibujo dTanque("./assets/images/canon.txt");
+    Dibujo dAlien("./assets/images/canon.txt");
+    Dibujo dBloque("./assets/images/canon.txt");
 
-   archivo.open("./assets/images/alien.txt")
-    string alien;
-    archivo>>alien;
-    archivo.close();
-    
     int fotograma=0;
     while(true) 
     {
     fotograma++;
     Element personaje = spinner(21,fotograma) | bold|color(Color::Blue1|bgcolor(Color::Aquiamarine);
-    Element tanque = text(canon) | bold | color(color::Green) | bgcolor(Color::Blue); 
+    Element tanque = dTanque.GetElement() | bold | color(color::Green) | bgcolor(Color::Blue); 
     Element lienzo = hbox({personaje , tanque });
     
     Screen pantalla = screen :: Create(
         Dimension::Full(),
-        Dimension::Full()
+        Dimension::Fit(lienzo));
 
         Render(pantalla,lienzo);
         pantalla.Print();
         cout<<pantalla.ResetPosition();
-    )
+
+
+    this_thread::sleep_for(0.1s);
     }
+    return 0;
 }
 
